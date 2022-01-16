b397c913d768703fdd2ac1b5a993fabc2273b09c
unlimited1974@aol.com <unlimited1974@aol.com>
2022-01-16 16:20:46 -0600

    commit tree 9bc1a40d37b119164a517be33b7347b9fc60a57d

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ main.remove();
 
 const newHeader = document.createElement("h1");
 newHeader.id = "victory";
-newHeader.innerHTML = "Tony is the champion"
+newHeader.innerHTML = "Tony is the champion again!"
 document.body.append(newHeader);
 
